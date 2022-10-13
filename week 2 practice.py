f026aa4839c95dd210186d197af403ce8076af05
hamzazkhan2 <115165729+hamzazkhan2@users.noreply.github.com>
2022-10-13 17:24:56 +0100

    Add files via upload

---
+++ week 2 practice.py
@@ -0,0 +1,61 @@
+print("hello world")
+total = 100
+print ("the total is", total)
+total += 99
+print ("the total is now", total)
+total -= 1
+print("the total is now", total)
+print("The total is", total)
+
+total *=  4
+print("The total is", total)
+
+total /= 2
+print("The total is", total)
+
+total = 98.2
+count = 5
+average = total/count
+print (average)
+
+print (type (1000))
+print (type(1000))
+print (type(100.111))
+print (type("hello"))
+print (type(True))
+print (type(100/5))
+
+name = "Hamza"
+print("the length of your name is", len(name))
+
+
+
+pi = "3.14"
+print (pi)
+
+name = input("whats your name?")
+print ("hello there", name)
+
+age = input("enter your age")
+age = int(age)
+print("in one year you will be", age+1)
+
+print("this text includes \\ \" and \'")
+
+print ("\\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \\ \nHello there!\n \\ \\ \\ \\ \\ ")
+
+print("""this text spans three lines
+and includes both single (')
+and double quotes(")""")
+
+surname = "khan"
+middle = surname[1:3]
+print(middle)
+
+primes = [2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+middle = primes[0:5]
+print (middle)
+
+nums = [1,2,3]*5
+print (nums)
+
